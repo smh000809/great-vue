8130ed8abf612eefc8daae97e401ad1b571cf09a
smh000809 <smh000809@outlook.com>
2023-11-17 14:39:13 +0800

    lint-fix

--- packages/json-sort/index.js
+++ packages/json-sort/index.js
@@ -37,14 +37,12 @@ const loadJson = async ({jsonPath, outputPath, fileName, relativePath}) => {
       const _sortedJson = JSON.stringify(sortedJson, null, 2)
 
       // 将排序后的数据写入文件
-      writeFile(outputFilePath, _sortedJson, 'utf8', (err) => {
+      writeFile(outputFilePath, _sortedJson, 'utf8', err => {
         if (err) {
           error(err)
           return
         }
-        log(
-          `${fileName} -> 排序完成，${jsonData.length === Object.keys(sortedJson).length ? '数据完整' : '数据不完整'}`,
-        )
+        log(`${fileName} -> 排序完成，${jsonData.length === Object.keys(sortedJson).length ? '数据完整' : '数据不完整'}`)
       })
     } catch (err) {
       error('无法解析 JSON 数据:', err)
@@ -56,11 +54,11 @@ const startLoad = async (relativePath = '/src/i18n/modules') => {
   log(`寻找目录：${relativePath}`)
   // 获取 src/i18n/modules 目录下的所有文件
   const languagesFinder = resolve($dirname, `./${relativePath}`)
-  const languages = readdirSync(languagesFinder).filter((v) => v.endsWith('.json'))
+  const languages = readdirSync(languagesFinder).filter(v => v.endsWith('.json'))
   log(`找到${languages.length}个 JSON 文件`)
   log('开始排序...'.repeat(2))
   log('*'.repeat(30))
-  const allLanguages = languages.map((language) => {
+  const allLanguages = languages.map(language => {
     return {
       jsonPath: `${languagesFinder}/${language}`,
       outputPath: `${languagesFinder}/${language}`,
@@ -69,7 +67,7 @@ const startLoad = async (relativePath = '/src/i18n/modules') => {
     }
   })
   await Promise.all(
-    allLanguages.map(async (language) => {
+    allLanguages.map(async language => {
       await loadJson({
         jsonPath: language.jsonPath,
         outputPath: language.outputPath,
